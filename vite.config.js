4d7a8b1045623e6d57c6ad8ad64807f6936906d3
Erin Bleiweiss <erinbleiweiss@gmail.com>
2026-05-01 00:30:55 -0500

    image test

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
 // theme/vite.config.js
 import { defineConfig } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
-        vue(),
-        // viteStaticCopy({
-        //     targets: [
-        //         {
-        //             src: './static',
-        //             dest: './jsonresume-theme-rikkipedia/static'
-        //         }
-        //     ]
-        // })
+        vue()
     ],
     ssr: {
         // This ensures Vue isn't treated as an external CJS module
