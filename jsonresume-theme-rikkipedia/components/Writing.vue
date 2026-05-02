98a6e43ac12d0c2e1bb8252da538d0ffb4f3cc6e
Erin Bleiweiss <erinbleiweiss@gmail.com>
2026-05-01 23:48:25 -0500

    add external link icons

--- jsonresume-theme-rikkipedia/components/Writing.vue
+++ jsonresume-theme-rikkipedia/components/Writing.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="writing" v-if="writing && writing.length > 0">
     <h4>Writing</h4>
-    <div v-for="item of writing">
-      <a :href="`/static/${encodeURIComponent(item.file)}`" target="_blank">{{item.title}}</a>
+    <div v-for="item of writing" class="item">
+      <a :href="`/static/${encodeURIComponent(item.file)}`" target="_blank">{{item.title}} <i class="fa-solid fa-arrow-up-right-from-square"></i> </a>
       <vue-markdown v-if="item.description" :source="item.description" class="description" :options="{ html: true }"/>
     </div>
   </div>
