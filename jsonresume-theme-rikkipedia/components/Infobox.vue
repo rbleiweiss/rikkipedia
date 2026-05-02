8a368b3b3fc8392b5fc1bd2d49b7175d0670e4f7
Erin Bleiweiss <erinbleiweiss@gmail.com>
2026-05-02 01:04:15 -0500

    infobox

--- jsonresume-theme-rikkipedia/components/Infobox.vue
+++ jsonresume-theme-rikkipedia/components/Infobox.vue
@@ -1,31 +1,34 @@
 <template>
-  <div class="infobox">
-    <div class="header">{{resume.basics.name}}</div>
-    <div class="content">
-      <img src="/static/headshot.png"/>
-      <div class="about-container">
-        <table class="about-table">
-          <tr v-if="resume.basics.location">
-            <td>Lives</td>
-            <td>{{ formatAddress(resume.basics.location) }}</td>
-          </tr>
+  <div class="infobox-container">
+    <div class="infobox">
+      <div class="header">{{resume.basics.name}}</div>
+      <div class="content">
+        <img src="/static/headshot.png"/>
+        <div v-if="resume.basics.caption" class="caption">{{resume.basics.caption}}</div>
+        <div class="about-container">
+          <table class="about-table">
+            <tr v-if="resume.basics.location">
+              <td>Lives</td>
+              <td>{{ formatAddress(resume.basics.location) }}</td>
+            </tr>
 
-          <tr v-if="resume.education && resume.education.length > 0">
-            <td>Education</td>
-            <td>
-              <div v-for="edu of resume.education">
-                {{edu.studyType}} in {{edu.area}} from {{edu.institution}}
-              </div>
-            </td>
-          </tr>
+            <tr v-if="resume.education && resume.education.length > 0">
+              <td>Education</td>
+              <td>
+                <div v-for="edu of resume.education" class="item">
+                  {{edu.studyType}} in {{edu.area}} from {{edu.institution}}
+                </div>
+              </td>
+            </tr>
 
-          <tr v-if="resume.skills && resume.skills.length > 0">
-            <td>Skills</td>
-            <td>
-              <div v-for="skill of resume.skills">{{skill}}</div>
-            </td>
-          </tr>
-        </table>
+            <tr v-if="resume.skills && resume.skills.length > 0">
+              <td>Skills</td>
+              <td>
+                <div class="item">{{getSkills(resume)}}</div>
+              </td>
+            </tr>
+          </table>
+        </div>
       </div>
     </div>
   </div>
@@ -47,6 +50,15 @@ const formatAddress = function(location) {
   })
   return addressList.join('<br/>');
 }
+
+const getSkills = function(resume){
+  let skills = []
+  if (resume.skills.length > 0) {
+    skills = resume.skills.join(", ")
+  }
+  return skills
+}
+
 </script>
 
 <style lang="scss" scoped>
