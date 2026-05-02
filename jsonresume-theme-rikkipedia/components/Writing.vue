75d7d54a776b3993134c94d37f1f8fcf9372fa7e
Erin Bleiweiss <erinbleiweiss@gmail.com>
2026-05-01 23:59:03 -0500

    Support external links in portfolio

--- jsonresume-theme-rikkipedia/components/Writing.vue
+++ jsonresume-theme-rikkipedia/components/Writing.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="writing" v-if="writing && writing.length > 0">
-    <h4>Writing</h4>
+    <h3 class="bold">Writing</h3>
     <div v-for="item of writing" class="item">
-      <a :href="`/static/${encodeURIComponent(item.file)}`" target="_blank">{{item.title}} <i class="fa-solid fa-arrow-up-right-from-square"></i> </a>
+      <a :href="externalUrl(item)" target="_blank">{{item.title}} <i class="fa-solid fa-arrow-up-right-from-square"></i> </a>
       <vue-markdown v-if="item.description" :source="item.description" class="description" :options="{ html: true }"/>
     </div>
   </div>
@@ -12,6 +12,15 @@
 import VueMarkdown from 'vue-markdown-render'
 
 const props = defineProps(['writing'])
+
+const externalUrl = function(item){
+  if (item.file){
+    return `/static/${encodeURIComponent(item.file)}`
+  } else if (item.url) {
+    return item.url
+  }
+}
+
 </script>
 
 <style lang="scss" scoped>
